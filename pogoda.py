3fc24ae32856b0b20395923aad4fa434c21aed29
Piotr <piotr_jasnowski@o2.pl>
2021-06-21 16:39:54 +0200

    Zapisywanie do pliku .txt

--- pogoda.py
+++ pogoda.py
@@ -16,6 +16,14 @@ class Weather:
     def __str__(self):
         return f'Miasto: {self.name},Temperatura: {self.temp_c},Data: {self.localtime}'
 
+    def write_txt(self):
+        with open("Weather.txt", "w") as file:
+            file.write(f'{self.name},{self.temp_c},{self.localtime}')
+
+    def reader_txt(self):
+        with open("Weather.txt", "r") as read_file:
+            read_file.readline()
+
 
 class GetData:
     def __init__(self, url):
@@ -24,11 +32,16 @@ class GetData:
 
     def get_data(self):
         r = requests.get(self.url)
+        h = r.headers
+        for date in h.items():
+            if 'Date' in date:
+                print(date[1])
         return r.json()
+weather = Weather
+
 # 'http://api.weatherapi.com/v1/current.json?key=0e7e4398f1164df2827105246212106&q=Amsterdam&aqi=no'
 city = input("City: ")
 data = GetData(f'http://api.weatherapi.com/v1/current.json?key=0e7e4398f1164df2827105246212106&q={city}&aqi=no')
-# pprint.pprint(data.data)
 
 for entry in data.data.items():
         information.append(entry)
@@ -48,3 +61,4 @@ for word_two in information[1]:
 
 weather = Weather(show_info[0], show_info[2], show_info[1])
 print(weather)
+weather.write_txt()
